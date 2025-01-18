d2f9b6fc30983c00d29069d3db4fda505f235644
ElieMitri <eliegmitri7@gmail.com>
2025-01-18 18:46:54 +0200

    test3

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
+      <Navbar />
       <LandingPage />
     </div>
   );
